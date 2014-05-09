81dc4d170ab4e62f978eb0fd667141bce0bb4f66
João Azevedo <jlazevedo92@gmail.com>
2014-05-09 21:14:30 +0100

    descriptions and movie_year added

---
+++ IMDB_descriptions.py
@@ -0,0 +1,38 @@
+import requests
+import simplejson
+from rottentomatoes import RT
+import imdb
+
+
+# create a single node
+def imdbDescriptions(filename):
+
+    ia = imdb.IMDb()
+
+    f = open(filename, 'r')
+    l = open('descriptions.txt', 'a')
+    for z, line in enumerate(f):
+        print z
+        values = line.split('|')
+
+        imdbID = values[3]
+
+        try:
+            x = ia.get_movie(imdbID)['plot outline']
+            print x
+
+
+            print >>l, x.encode('utf-8').strip()
+
+        except KeyError, e:
+            print >>l, ''
+
+
+
+    f.close()
+    l.close()
+
+
+
+if __name__ == "__main__":
+    imdbDescriptions('finalfile2.txt')
--- addMovies.py
+++ addMovies.py
@@ -1,4 +1,4 @@
-import numpy
+import re
 from py2neo import neo4j, node, rel
 
 graph_db = neo4j.GraphDatabaseService()
@@ -18,35 +18,43 @@ def createNodesfromMovieFile(filename):
 
     nodes = []
 
-    with open(filename, 'r') as f1, open('images.txt', 'r') as f2, open('adjectives.txt', 'r') as f3 :
-        for x, y, d in zip(f1, f2, f3):
+    array = []
+
+    with open(filename, 'r') as f1, open('images.txt', 'r') as f2, open('adjectives.txt', 'r') as f3, open('descriptions.txt', 'r') as f4 :
+        for x, y, d, o in zip(f1, f2, f3, f4):
 
             print x,
+
+            q = re.sub('[\n [\]\']', '', d).split(',')
+
+            if len(q) == 1 and q[0] == '':
+                q = []
+
+            print q
+            array.append(q)
 
             #print z
             values = x.split('|')
 
             ident = values[0]
-
-            if ident == '267' or ident == '1358' or ident == '1359':
-                continue
-
-
             title = values[1]
+            try:
+                year = int(values[2])
+            except ValueError, e:
+                year = None
             imdb = values[3]
             genre = values[4:]
             movie_genres = []
             img = y[:-1]
 
+            movie = batch.get_or_create_in_index(neo4j.Node, 'Movie', 'movie_id', ident,
+                    node({'movie_id': ident, 'title': unicode(title, errors='ignore'),
+                          'imdb': imdb, 'img': img, 'year': year, 'description': unicode(o.replace('\n',''),errors='ignore')}))
+
             for i, n in enumerate(map(int, genre)):
                 if n == 1:
                     movie_genres.append((i, batch.get_or_create_in_index(neo4j.Node, 'Genre', 'genre_id', i, node({'genre_id': int(i), 'name': genres[i]}))))
                     aux += 1
-            genrs_array.append(aux)
-            aux = 0
-
-            movie = batch.get_or_create_in_index(neo4j.Node, 'Movie', 'movie_id', ident, node({'movie_id': ident, 'title': unicode(title, errors='ignore'), 'imdb': imdb, 'img': img}))
-
 
 
             for gen_id, gen in movie_genres:
@@ -61,32 +69,27 @@ def createNodesfromMovieFile(filename):
 
             z += 1
 
+        w = 0
+
+        print len(array)
+
         for e, n in enumerate(nodes):
             if e == 0:
                 print n
+                for a in array[w]:
+                    batch.add_labels(n, a)
+                w += 1
             elif genrs_array[0] == 0:
                 print n
-                if e > 10:
-                    break
-                #batch.add_labels(node, d)
+                for a in array[w]:
+                    batch.add_labels(n, a)
                 genrs_array = genrs_array[1:]
+                w += 1
             else:
+                print -1
                 genrs_array[0] -= 1
 
-
-        try:
-
-            #d = d.replace('[','')
-            #d = d.replace(']','')
-
-            batch.add_labels(movie, d)
-
-
-        except AttributeError, e:
-            pass
-
-
-
+    batch.submit()
 
     print "ENDED"
 
